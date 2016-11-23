1214c55ddc8014f8addf74f0a0f5ef3379f5c377
Oblitus <Oblitus@LAPTOP-B0T741JU>
2016-11-23 06:07:38 +0100

    Added Class AU_MouthRect_Rene.cs

---
+++ Modules/AU_MouthRect_Rene.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace RealSense.Modules
+{
+    /**
+     * This class detects whether the mouth forms an rectangle or not 
+     */
+    class AU_MouthRect_Rene : RSModule
+    {
+        private float[] lmArray = new float[12];
+
+        public override void Work(Graphics g)
+        {
+            initLandmarks();
+            findRect();
+        }
+
+
+        /*
+         * Checks whether the mouth is an rectangle
+         */
+        private void findRect()
+        {
+            //Das macht geometrisch alles keinen sinn :|
+        }
+
+
+        /**
+         * Initiates mouthLandmarks by 
+         */
+        public void initLandmarks()
+        {
+            if (model.FaceAktuell == null) return;
+            PXCMFaceData.LandmarksData lp = model.FaceAktuell.QueryLandmarks();
+            PXCMFaceData.LandmarkPoint point;
+
+            if (lp != null)
+                for (int i = 35; i < 11; i++)
+                {
+                    int idx = 0;
+                    lp.QueryPoint(i, out point);
+                    lmArray[idx] = point.world.y;
+                    idx++;
+
+                }
+
+        }
+    }
+}
