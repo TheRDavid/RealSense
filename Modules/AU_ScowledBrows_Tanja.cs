c9a4cd6efe4e51fb22c999b0aca44ebab44b6eb9
Tanja <Tanja@TANJA-MOBIL>
2016-12-07 15:46:11 +0100

    bla

--- Modules/AU_ScowledBrows_Tanja.cs
+++ Modules/AU_ScowledBrows_Tanja.cs
@@ -20,7 +20,6 @@ namespace RealSense
 
             if (augenbrauenAbstand < 99 && augenBraueNaseAbstand < 90)
             {
-                g.DrawString("scrowled Brows", arialFont, redBrush, new PointF(20, 30));
                 model.Anger += 50;
             }                
         }
