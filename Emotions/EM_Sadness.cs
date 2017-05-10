d52baa14a09faebab047f52a2eb6086e53ecfcdb
Tanja <Tanja@TANJA-MOBIL>
2017-05-10 18:33:40 +0700

    Sadness erster Versuch

--- Emotions/EM_Sadness.cs
+++ Emotions/EM_Sadness.cs
@@ -4,21 +4,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 
-
-namespace RealSense
+namespace RealSense.Emotions
 {
-    /*
-     *Measures if complete brow is raised or lowered (each eye)  - Action Unit Number 4 
-     *@author Anton 
-     *@date 20.03.2017
-     *@HogwartsHouse Slytherin  
-     */
-    class EM_Anger : RSModule
+    class EM_Sadness : RSModule
     {
         // Variables for logic
 
         // Default values
-        public EM_Anger()
+        public EM_Sadness()
         {
             debug = true;
         }
@@ -33,46 +26,47 @@ namespace RealSense
             9 = nose wrinkled -> NoseWrinkled
             12 = lip corner pulled (up) -> LipCorner
             14 = grübchen -> none
-            15 = lip corner lowered -> LipCorner
+            15 = lip corner lowered -> LipLine
             16 = lower lip lowered ->LowerLipLowered
             20 = lip stretched -> LipStretched
             23 = lip tightened -> LipsTightened
             26 = jaw drop -> JawDrap
+
+            Verachtung (12 (R,L), 14(R,L)
+            Trauer (1,4,15,(20?)
+            Wut (4,5,6,23)
+            Ekel (9,15,16,4)
+            Überraschung (1,2,5B,26)
+            Freude (6,12, 7)
+            Angst (1,2,4,5,6,20,26)
+
          * */
 
         public override void Work(Graphics g)
         {
-            //Anger 4+5+7+23 --> BrowShift, EyelidTight, LipsTightened
-
-            /**
-             * Anleitung: 
-             * 1. Welche ME werden benoetigt?
-             * 2. Prozenteinfluss der MEs festlegen
-             * 3. fuer jede ME folgende Berechnung
-             *  3.1 Werte aus dem Model holen
-             *  3.2 gegebenenfalls benoetigten Wert berechnen
-             *  3.3 bei negativen Werten mit -1 multiplizieren (aufpassen mit den Gegenwerten... diese schliessen die ME allerdings meistens aus und sind daher ok/berechtigt?)
-             *  3.4 mit Prozentanteil kombinieren (* Anteil / 100)
-             * 4. Werte addieren und ins Model schreiben
-             * 
-             * */
+            //Sadness --> BrowShift, LipLine, (LipStreched), EyelidTight
 
             //percentage Anger
             int p_brow = 40;
-            int p_lid = 30;
-            int p_lip = 30;
+            int p_lid = 20;
+            int p_lipL = 40;
+            //int p_lipS = 40;
+
+            //Line: hoch lächeln (60-90) (grinsen höher)
+            //Line bei traurig: -40/-50 Streched neutral
+
 
             //Lid too tight Vars
-            int lidMax = 90;
+            int lidMax = 50;
             int newLid = -10;
 
-            //brow Value
+            //brow Value 0-100
             double temp_left = model.getAU_Value(typeof(ME_BrowShift).ToString() + "_left");
             double temp_right = model.getAU_Value(typeof(ME_BrowShift).ToString() + "_right");
-            double browValue = temp_left > temp_right ? temp_left : temp_right;
-            browValue = browValue * -1 * p_brow / 100; 
+            double browValue = temp_left < temp_right ? temp_left : temp_right;
+            browValue = browValue * p_brow / 100;
 
-            //lid Value
+            //lid Value 0 - -100 (Grenze bei lidMax)
             temp_left = model.getAU_Value(typeof(ME_EyelidTight).ToString() + "_left");
             temp_right = model.getAU_Value(typeof(ME_EyelidTight).ToString() + "_right");
             double lidValue = temp_left > temp_right ? temp_left : temp_right;
@@ -80,20 +74,24 @@ namespace RealSense
             lidValue = temp_left > -lidMax || temp_right > -lidMax ? lidValue : newLid;
             lidValue = lidValue * -1 * p_lid / 100;
 
-            //lip Value
-            double lipValue = model.getAU_Value(typeof(ME_LipsTightened).ToString() + "_upperBottomLip");
-            lipValue = lipValue * -1 * p_lip / 100;
+            //lipL Value 0 - -100
+            double lipValue = model.getAU_Value(typeof(ME_LipLine).ToString());
+            lipValue = lipValue * -1 * p_lipL / 100;
 
-            double anger = browValue + lidValue + lipValue;
-            anger = anger > 0 ? anger : 0;
-            model.Emotions["Anger"] = anger;
+            double sad = browValue + lidValue + lipValue;
+            sad = sad > 0 ? sad : 0;
+            model.Emotions["Sadness"] = sad;
 
             // print debug-values 
             if (debug)
             {
-                output = "Anger: " + anger;
+                output = "Sadness: " + sad;
             }
 
         }
     }
 }
+
+
+
+
